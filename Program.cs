add3df81303032507d9a659b252028a20ba837a7
Taha2341 <86522804+Taha2341@users.noreply.github.com>
2021-07-27 17:16:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Döngüler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] {"Yazılım Geliştirme Kampı","Programlamaya Başlangıç İçin temel Kurs",
+                "java","python","C#"};
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For Bitti");
+
+            foreach (string kurs in kurslar)
+            {
+
+                Console.WriteLine(kurs);
+
+            }
+            Console.WriteLine("Sayfa sonu - footer");
+
+        }
+    }
+}
